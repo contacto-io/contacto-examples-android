f9d1077d4903fc711582ed1d74ada87178f6ad34
Sumit <sumitgupta.gupta14@gmail.com>
2022-08-12 10:20:29 +0530

    Edit profile

--- app/src/main/java/com/contacto/consumer/android/example/ui/App.kt
+++ app/src/main/java/com/contacto/consumer/android/example/ui/App.kt
@@ -22,4 +22,34 @@ class App : Application(), com.contacto.consumer.android.example.base.ICommon {
         val sharedPreferences: SharedPreferences = getSharedPreferences("ContacctoPref", MODE_PRIVATE)
         return sharedPreferences.getString("APP_ID", null)
     }
+
+    override fun saveUser(fName: String, lName: String, email: String, phone: String) {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("ContacctoPref", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("FIRST_NAME", fName)
+        editor.putString("LAST_NAME", lName)
+        editor.putString("EMAIL", email)
+        editor.putString("PHONE", phone)
+        editor.apply()
+    }
+
+    override fun getFirstName(): String? {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("ContacctoPref", MODE_PRIVATE)
+        return sharedPreferences.getString("FIRST_NAME", null)
+    }
+
+    override fun getLastName(): String? {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("ContacctoPref", MODE_PRIVATE)
+        return sharedPreferences.getString("LAST_NAME", null)
+    }
+
+    override fun getEmail(): String? {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("ContacctoPref", MODE_PRIVATE)
+        return sharedPreferences.getString("EMAIL", null)
+    }
+
+    override fun getPhone(): String? {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("ContacctoPref", MODE_PRIVATE)
+        return sharedPreferences.getString("PHONE", null)
+    }
 }
